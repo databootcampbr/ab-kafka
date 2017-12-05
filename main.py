92acd07d4a2ca68416f2c0afc52436a39cca0aae
Diogo Munaro <diogo.munaro@corp.globo.com>
2017-12-05 09:36:33 -0200

    Ab with amazon page as example

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_split import split, ab_test, finished
 
 app = Flask(__name__)
@@ -13,10 +13,14 @@ app.secret_key = 'super secret key'
 
 app.register_blueprint(split)
 @app.route("/")
-def hello():
-    ab_test('signup_btn_text', 'Register', 'SignUp')
-    finished('signup_btn_text')
-    return "Hello World!"
-
+def product():
+    alternative = ab_test('buy_text', 'Buy', 'Free')
+    return render_template('product.html',
+        alternative=alternative
+    )
+@app.route("/buy")
+def buy():
+    finished('buy_text')
+    return render_template('buy.html')
 if __name__ == "__main__":
     app.run()
